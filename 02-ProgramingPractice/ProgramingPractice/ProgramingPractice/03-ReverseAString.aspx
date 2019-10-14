077504de94f59173d29a754ac29df436d4dbdedb
Saqib Amin <saqibamin257@gmail.com>
2019-10-14 14:44:36 +0100

    ReverseOfString
    Reverse Of a string program

---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/03-ReverseAString.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="03-ReverseAString.aspx.cs" Inherits="ProgramingPractice._03_ReverseAString" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div runat="server" id="div1">
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/03-ReverseAString.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProgramingPractice
+{
+    public partial class _03_ReverseAString : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            div1.InnerText="Reverse of Hello is : "+ Reverse("Hello");
+        }
+
+        public string Reverse(string word)
+        {            
+            char[] charArray = word.ToCharArray();
+            char[] reverseArray =new char[charArray.Length];
+            for (int i = charArray.Length - 1,j=0; i >= 0; i--)
+            {
+                reverseArray[j] = charArray[i];
+                j++;
+            }
+            string str = new string(reverseArray);
+            return str;
+        }
+    }
+}
