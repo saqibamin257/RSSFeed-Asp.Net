46b88187758f4ee83f5d988f0c12863b4e674320
Saqib Amin <saqibamin257@gmail.com>
2019-10-15 09:45:44 +0100

    Palindrome program
    Check that string is plaindrome or not

---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/04-Palindrome.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="04-Palindrome.aspx.cs" Inherits="ProgramingPractice._04_Palindrome" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="div1" runat="server">
+        </div>
+    </form>
+</body>
+</html>
---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/04-Palindrome.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProgramingPractice
+{
+    public partial class _04_Palindrome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //level,anna,civic,
+            string str = "redder";
+            div1.InnerText = str + " is " + isPalindrome(str);
+        }
+
+        public string isPalindrome(string str)
+        {
+            string result = "Palindrome";
+            char[] charArray = str.ToCharArray();
+
+            for (int i = 0, j = charArray.Length - 1; i <= (charArray.Length - 1)/2; i++,j--)
+            {
+                if (charArray[i] != charArray[j])
+                {
+                    result = "Not Palindrome";
+                }
+            }
+
+            return result;
+        }
+    }
+}
