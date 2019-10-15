6c9b2b339fe621b223682c039efb83063fa60e23
Saqib Amin <saqibamin257@gmail.com>
2019-10-15 13:28:02 +0100

    CountNumberOfOccuranceOfCharacter
    Count total number
    of occurance of each character in string

---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/05-CountOccuranceOfCharacter.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="05-CountOccuranceOfCharacter.aspx.cs" Inherits="ProgramingPractice._05_CountOccuranceOfCharacter" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div >
+            Number of occurence for each character in Hello world is :<br/>
+            <p id="p1" runat="server">
+
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/05-CountOccuranceOfCharacter.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProgramingPractice
+{
+    public partial class _05_CountOccuranceOfCharacter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string str = "Hello World";
+            ArrayList ts = Occurance(str);
+            foreach (var s in ts)
+            {
+                p1.InnerText += s.ToString();
+            }
+        }
+        public ArrayList Occurance(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            
+            ArrayList arryList1 = new ArrayList();
+            ArrayList arryList2 = new ArrayList();
+            for (int i = 0; i <= charArray.Length - 1; i++)
+            {
+                int count = 0;
+                for (int j=i; j<=charArray.Length-1; j++)
+                {
+                    if (charArray[i] == charArray[j])
+                    {
+                        count++;                        
+                    }                    
+                }
+                if (!arryList2.Contains(charArray[i]))
+                {
+                    arryList2.Add(charArray[i]);
+                    arryList1.Add(charArray[i] + " = " + count + " -times. \n");
+                }
+            }
+
+            return arryList1;
+        }
+    }
+}
