be40cb3dfc24d59ab4b1a74b87b1614f743b1f13
Saqib Amin <saqibamin257@gmail.com>
2020-03-20 11:14:00 +0000

    MutliThreading

---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/06-MultiThreading.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="06-MultiThreading.aspx.cs" Inherits="ProgramingPractice._06_MultiThreading" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">        
+        <div>
+            <asp:Button runat="server" Text="Single Thread Process" OnClick="SingleThread_Click" />
+        </div>
+        <div>
+            <asp:Button runat="server" Text="Multi Thread Process" OnClick="MultiThread_Click" />
+        </div>
+        <p runat="server" id="p2">
+          
+        </p>
+    </form>
+</body>
+</html>
---
+++ 02-ProgramingPractice/ProgramingPractice/ProgramingPractice/06-MultiThreading.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProgramingPractice
+{
+    public partial class _06_MultiThreading : System.Web.UI.Page
+    {
+        public void FirstThread()
+        {            
+            for (int i = 1; i <= 10; i++)
+            {
+                p2.InnerHtml += $"M1 - {i} <br/>";
+                //Console.WriteLine("M1 is : {0} ", i);
+                if (i == 5)
+                {
+                    Thread.Sleep(6000);
+                }
+            }
+        }
+        public void SecondThread()
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                p2.InnerHtml += $"M2 - {j} <br/>";
+                //Console.WriteLine("M2 is : {0} ", j);
+
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            p2.InnerHtml = "";
+
+        }
+
+        protected void SingleThread_Click(object sender, EventArgs e)
+        {
+            FirstThread();
+            SecondThread();
+        }
+        protected void MultiThread_Click(object sender, EventArgs e)
+        {
+            Thread th1 = new Thread(FirstThread);
+            Thread th2 = new Thread(SecondThread);
+            th1.Start();
+            th2.Start();
+        }
+
+    }
+}
